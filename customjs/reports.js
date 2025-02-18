f51b21839b20586882413e8018033e5d7ef2285d
fionachristina <fionachristina6@gmail.com>
2025-02-18 15:53:36 +0300

    Working on reports

---
+++ customjs/reports.js
@@ -0,0 +1,7 @@
+'use strict'
+
+
+const portfolioList = document.getElementById('portfolio-list');
+
+
+console.log(portfolioList)
---
+++ images-copy/reports/index.js
@@ -0,0 +1,26 @@
+export const GoodStart = require("./A-Good-Start–LifeSkillsComponent–2021.jpg");
+export const AnnualReport2019 = require("./annual-report-2019.jpg");
+export const AnnualReport2020 = require("./annual-report-2020.jpg");
+export const CSCC = require("./Capacity-Strengthening-and-Sharing-Course-2021.jpg");
+export const CohereStrategicPlan = require("./CohereStrategicPlan2022-2026.webp");
+export const DataProtectionPolicy = require("./Data-Protection-Policy-2018.jpg");
+export const ProcurementUganda = require("./procument-uganda.jpg");
+export const SafeGuardingPolicy = require("./safeguarding-policy-2021.jpg");
+export const SEParents = require("./Social-and-Emotional-Learning-Course-for-Parents.jpeg");
+export const SETools = require("./Social-and-Emotional-Learning-Tools-2019.jpg");
+export const Limits = require("./The-Limits-to-Universal-Education.jpg");
+export const XPAnnualReport2016 = require("./Xavier-Project-Annual-Report-2016.jpg");
+export const XPAnnualReport2017 = require("./Xavier-Project-Annual-Report-2017.jpg");
+export const XPAnnualReport2018 = require("./Xavier-Project-Annual-Report-2018.jpg");
+export const XPChildProtection = require("./Xavier-Project-Child-Protection-Policy-2021.jpg");
+export const XPStrategicPlan18 = require("./Xavier-Project-Strategic-Plan-2015-2018.jpg");
+export const XPStrategicPlan21 = require("./Xavier-Project-Strategic-Plan-2019-2021.jpg");
+
+export const AAPImage = require("./Accountability-of-affected-persons.png");
+export const refugeeParticipationReportImg = require("./refugee-participation-report.jpg");
+export const refugeeParticipationReportExecSumImg = require("./refugee-participation-report-exec-sum.jpg");
+
+export const fiveOrganisationalPathwaysImg = require("../organisationalPathways/organisational_pathways_landing.png");
+export const fiveOrganisationalPathwaysImgTwo = require("./Cohere_FB_IG_LinkedIn_Posts.png");
+
+export const impactReport = require("./impact-report-2021-2023.png");
