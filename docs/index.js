32e380611ba5b04bc92d340c2063a37e4efbc670
fionachristina <fionachristina6@gmail.com>
2025-03-27 11:56:48 +0300

    Updated homepage, about, reports

---
+++ docs/index.js
@@ -0,0 +1,8 @@
+export const CHStrategicPlan22_26 = require("./CohereStrategicPlan22-26.pdf");
+export const AAP = require("./Accountability to Affected Persons(AAP-Framework).docx.pdf");
+// export const RefugeeParticipationReport = require("./RefugeeParticipationReport_260823FINAL.pdf");
+export const RefugeeParticipationReport = require("./RefugeeParticipationReport_151123FINAL.pdf");
+export const RefugeeParticipationReportExecSummary = require("./RefugeeParticipationReport_EXECSUMMARY260823.pdf");
+export const ThreePathways = require("./Three_Pathways-Internal_Equity_Learning_Journey.pdf");
+export const organisationalPathwaysDoc = require("./Cohere_report_July12.pdf");
+export const impactHighlights = require("./CohereImpactHighlightsReport21-23.pdf");
